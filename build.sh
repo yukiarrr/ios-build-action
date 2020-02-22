af9e84be3dbe81503f0cc600d13d5fcd95816ef7
Yuki Araikawa <mail@yukiarrr.com>
2020-02-22 15:21:28 +0900

    Fix output path of p12 and mobileprovision

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-echo $P12_BASE64 | base64 --decode > ios-build.p12
-echo $MOBILEPROVISION_BASE64 | base64 --decode > ios-build.mobileprovision
-
 script_path=$(cd $(dirname ${0}); pwd)
 cd $(dirname ${PROJECT_PATH})
 cp -r ${script_path}/fastlane ./
+
+echo $P12_BASE64 | base64 --decode > ios-build.p12
+echo $MOBILEPROVISION_BASE64 | base64 --decode > ios-build.mobileprovision
 
 fastlane export_ipa
